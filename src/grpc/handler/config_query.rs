4099793e18d0c7a8a668dca4bf61d45e80bd13ba
heqingpan <heqingpan@126.com>
2022-10-30 13:22:44 +0800

    update ConfigQueryRequestHandler

--- src/grpc/handler/config_query.rs
+++ src/grpc/handler/config_query.rs
@@ -1,6 +1,6 @@
 #![allow(unused_imports)]
 
-use crate::{grpc::{PayloadHandler, api_model::{ConfigPublishRequest, BaseResponse, ConfigQueryRequest, ConfigQueryResponse}, nacos_proto::Payload, PayloadUtils}, config::config::{ConfigActor, ConfigCmd, ConfigKey, ConfigResult}};
+use crate::{grpc::{PayloadHandler, api_model::{ConfigPublishRequest, BaseResponse, ConfigQueryRequest, ConfigQueryResponse, SUCCESS_CODE, ERROR_CODE}, nacos_proto::Payload, PayloadUtils}, config::config::{ConfigActor, ConfigCmd, ConfigKey, ConfigResult}};
 use actix::prelude::Addr;
 use async_trait::async_trait;
 
@@ -20,28 +20,31 @@ impl PayloadHandler for ConfigQueryRequestHandler {
         let body_vec = request_payload.body.unwrap_or_default().value;
         let request:ConfigQueryRequest = serde_json::from_slice(&body_vec)?;
         let cmd = ConfigCmd::GET(ConfigKey::new(&request.data_id,&request.group,&request.tenant));
-        let response_str = match self.config_addr.send(cmd).await{
+        let mut response = ConfigQueryResponse::default();
+        match self.config_addr.send(cmd).await{
             Ok(res) => {
                 //let res:ConfigResult = res.unwrap();
                 let r:ConfigResult = res.unwrap();
                 match r {
                     ConfigResult::DATA(content) => {
                         //v.to_owned()
-                        let mut response = ConfigQueryResponse::default();
+                        response.result_code = SUCCESS_CODE;
                         response.content = content;
                         response.tag = request.tag;
-                        serde_json::to_string(&response)?
                     },
                     ConfigResult::NULL => {
-                        //"config data not exist".to_owned()
-                        serde_json::to_string(&BaseResponse::build_error_response(500u16,"config data not exist".to_owned()))?
+                        response.result_code = ERROR_CODE;
+                        response.error_code = ERROR_CODE;
+                        response.message = Some("config data not exist".to_owned());
                     }
                 }
             },
             Err(err) => {
-                serde_json::to_string(&BaseResponse::build_error_response(500u16,err.to_string()))?
+                response.result_code = ERROR_CODE;
+                response.error_code = ERROR_CODE;
+                response.message = Some(err.to_string());
             }
         };
-        Ok(PayloadUtils::build_payload("ConfigQueryResponse", response_str))
+        Ok(PayloadUtils::build_payload("ConfigQueryResponse", serde_json::to_string(&response)?))
     }
 }
