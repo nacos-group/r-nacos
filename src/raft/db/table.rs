4e094059c961d1817914f6220635176d6850cb2a
heqingpan <heqingpan@126.com>
2023-10-11 01:15:31 +0800

    TableManage add insert remove

--- src/raft/db/table.rs
+++ src/raft/db/table.rs
@@ -1,6 +1,6 @@
 use std::{sync::Arc, collections::HashMap};
 
-use crate::common::sled_utils::TableSequence;
+use crate::common::{sled_utils::TableSequence, byte_utils::id_to_bin};
 
 use serde::{Deserialize, Serialize};
 
@@ -64,13 +64,47 @@ impl TableManage {
         }
     }
 
-    pub(crate) fn init_table(&mut self,name: Arc<String>,seq_step: u32) {
+    pub fn init_table(&mut self,name: Arc<String>,seq_step: u32) {
         if !self.table_map.contains_key(&name) {
             self.table_map.insert(name.clone(),self.build_table_info(name.clone(), seq_step));
-            //let tables = self.db.open_tree("tables").unwrap();
-            //tables.insert(name, value)
+            let tables = self.db.open_tree("tables").unwrap();
+            let definition = TableDefinition {
+                name:name.as_ref().to_owned(),
+                sequence_step:seq_step
+            };
+            tables.insert(name.as_bytes(), definition.to_bytes()).unwrap();
         }
     }
 
-    //pub fn insert()
+    pub fn drop_table(&mut self,name: &Arc<String>) {
+        if self.table_map.contains_key(name) {
+            self.table_map.remove(name);
+            //todo remove table data and seq
+        }
+    }
+
+    pub fn insert<K>(&mut self,name: Arc<String>, key: K ,value: Vec<u8>) -> Result<Option<sled::IVec>, sled::Error>
+        where K: AsRef<[u8]>
+    {
+        let table = self.db.open_tree(name.as_ref()).unwrap();
+        table.insert(key, value)
+    }
+
+    pub fn insert_with_auto_id(&mut self,name: Arc<String>,value: Vec<u8>) -> Result<Option<sled::IVec>, sled::Error> {
+        let id = if let Some(table_info)=self.table_map.get_mut(&name) {
+            table_info.seq.as_mut().unwrap().next_id().unwrap()
+        }
+        else{
+            panic!("the table is empty {}",&name)
+        };
+        self.insert(name, id_to_bin(id), value)
+
+    }
+
+    pub fn remove<K>(&mut self,name: Arc<String>, key: K) -> Result<Option<sled::IVec>, sled::Error>
+        where K: AsRef<[u8]>
+    {
+        let table = self.db.open_tree(name.as_ref()).unwrap();
+        table.remove(key)
+    }
 }
