27fb5d41ea1b858b6becc4e41138d553edc1a2b9
heqingpan <heqingpan@126.com>
2023-10-11 09:09:50 +0800

    impl Handler<TableManageCmd> for TableManage

--- src/raft/db/table.rs
+++ src/raft/db/table.rs
@@ -1,30 +1,42 @@
 use std::{sync::Arc, collections::HashMap};
 
-use crate::common::{sled_utils::TableSequence, byte_utils::id_to_bin};
-
 use serde::{Deserialize, Serialize};
+
+use actix::prelude::*;
 
 #[derive(Clone, prost::Message, Serialize, Deserialize)]
 pub struct TableDefinition {
     #[prost(string, tag = "1")]
     pub name : String,
-    #[prost(uint32, tag = "2")]
-    pub sequence_step: u32, // 0: None,  1: seq , _:step seq
 }
 
 impl TableDefinition {
     pub fn to_bytes(&self) -> Vec<u8> {
         let mut v = Vec::new();
-        //self.encode(&mut v).unwrap();
         prost::Message::encode(self,&mut v).unwrap();
         v
     }
+
+    pub fn from_bytes(v: &[u8]) -> anyhow::Result<Self> {
+        Ok(prost::Message::decode(v)?)
+    }
 }
 
+pub(crate) const TABLE_DEFINITION_TREE_NAME: &str = "tables";
 
 pub struct TableInfo {
     pub name: Arc<String>,
-    pub seq: Option<TableSequence>,
+    pub table_name: String,
+}
+
+impl TableInfo {
+    pub fn new(name: Arc<String>) -> Self {
+        let table_name = format!("t_{}",&name);
+        Self {
+            name,
+            table_name,
+        }
+    }
 }
 
 pub struct TableManage {
@@ -44,67 +56,128 @@ impl TableManage {
 
     /// load table info from db
     fn load_tables(&mut self) {
-        let tables = self.db.open_tree("tables").unwrap();
+        let tables = self.db.open_tree(TABLE_DEFINITION_TREE_NAME).unwrap();
         let mut iter = tables.iter();
         while let Some(Ok((_, v))) = iter.next() {
-            let definition :TableDefinition = prost::Message::decode(v.as_ref()).unwrap();
-            let name = Arc::new(definition.name.to_owned());
-            self.table_map.insert(name.clone() , self.build_table_info(name,definition.sequence_step));
+            if let Ok(definition) = TableDefinition::from_bytes(v.as_ref()) {
+                let name = Arc::new(definition.name.to_owned());
+                self.table_map.insert(name.clone() , TableInfo::new(name));
+            }
         }
     }
 
-    fn build_table_info(&self,name: Arc<String> ,seq_step: u32) -> TableInfo {
-        let seq = match seq_step {
-            0 => None,
-            _ => Some(TableSequence::new(self.db.clone(), format!("seq_{}",name.as_ref()), seq_step as u64))
+    fn init_table(&mut self,name: Arc<String>) {
+        let tables = self.db.open_tree(TABLE_DEFINITION_TREE_NAME).unwrap();
+        let definition = TableDefinition {
+            name:name.as_ref().to_owned(),
         };
-        TableInfo {
-            name,
-            seq,
-        }
-    }
-
-    pub fn init_table(&mut self,name: Arc<String>,seq_step: u32) {
-        if !self.table_map.contains_key(&name) {
-            self.table_map.insert(name.clone(),self.build_table_info(name.clone(), seq_step));
-            let tables = self.db.open_tree("tables").unwrap();
-            let definition = TableDefinition {
-                name:name.as_ref().to_owned(),
-                sequence_step:seq_step
-            };
-            tables.insert(name.as_bytes(), definition.to_bytes()).unwrap();
-        }
+        tables.insert(name.as_bytes(), definition.to_bytes()).unwrap();
     }
 
     pub fn drop_table(&mut self,name: &Arc<String>) {
-        if self.table_map.contains_key(name) {
-            self.table_map.remove(name);
-            //todo remove table data and seq
+        if let Some(table) = self.table_map.remove(name){
+            self.db.drop_tree(&table.table_name).ok();
         }
     }
 
-    pub fn insert<K>(&mut self,name: Arc<String>, key: K ,value: Vec<u8>) -> Result<Option<sled::IVec>, sled::Error>
+    pub fn insert<K>(&mut self,name: Arc<String>, key: K ,value: Vec<u8>) -> Option<sled::IVec>
         where K: AsRef<[u8]>
     {
-        let table = self.db.open_tree(name.as_ref()).unwrap();
-        table.insert(key, value)
-    }
-
-    pub fn insert_with_auto_id(&mut self,name: Arc<String>,value: Vec<u8>) -> Result<Option<sled::IVec>, sled::Error> {
-        let id = if let Some(table_info)=self.table_map.get_mut(&name) {
-            table_info.seq.as_mut().unwrap().next_id().unwrap()
+        if let Some(table_info) = self.table_map.get(&name) {
+            let table = self.db.open_tree(&table_info.table_name).unwrap();
+            table.insert(key, value).unwrap()
         }
         else{
-            panic!("the table is empty {}",&name)
-        };
-        self.insert(name, id_to_bin(id), value)
-
+            self.init_table(name.clone());
+            let table_info = TableInfo::new(name.clone());
+            let table = self.db.open_tree(&table_info.table_name).unwrap();
+            self.table_map.insert(name, table_info);
+            table.insert(key, value).unwrap()
+        }
     }
 
-    pub fn remove<K>(&mut self,name: Arc<String>, key: K) -> Result<Option<sled::IVec>, sled::Error>
+    pub fn remove<K>(&mut self,name: Arc<String>, key: K) -> Option<sled::IVec>
         where K: AsRef<[u8]>
     {
-        let table = self.db.open_tree(name.as_ref()).unwrap();
-        table.remove(key)
+        if let Some(table_info) = self.table_map.get(&name) {
+            let table = self.db.open_tree(&table_info.table_name).unwrap();
+            table.remove(key).unwrap()
+        }
+        else{
+            None
+        }
+    }
+}
+
+impl Actor for TableManage {
+    type Context = Context<Self>;
+}
+
+#[derive(Message)]
+#[rtype(result = "anyhow::Result<TableManageResult>")]
+pub enum TableManageAsyncCmd {
+    Insert{
+        table_name: Arc<String>,
+        key: Vec<u8>,
+        value: Vec<u8>,
+    },
+    Remove{
+        table_name: Arc<String>,
+        key: Vec<u8>,
+    },
+    Drop(Arc<String>),
+}
+
+#[derive(Message)]
+#[rtype(result = "anyhow::Result<TableManageResult>")]
+pub enum TableManageCmd {
+    Insert{
+        table_name: Arc<String>,
+        key: Vec<u8>,
+        value: Vec<u8>,
+    },
+    Remove{
+        table_name: Arc<String>,
+        key: Vec<u8>,
+    },
+    Drop(Arc<String>),
+}
+
+pub enum TableManageResult {
+    None,
+    Value(Vec<u8>)
+}
+
+
+impl Handler<TableManageAsyncCmd> for TableManage {
+    type Result = ResponseActFuture<Self, anyhow::Result<TableManageResult>>;
+
+    fn handle(&mut self, msg: TableManageAsyncCmd, ctx: &mut Self::Context) -> Self::Result {
+        todo!()
+    }
+}
+
+impl Handler<TableManageCmd> for TableManage {
+    type Result = anyhow::Result<TableManageResult>;
+
+    fn handle(&mut self, msg: TableManageCmd, _ctx: &mut Self::Context) -> Self::Result {
+        match msg {
+            TableManageCmd::Insert { table_name, key, value } => {
+                match self.insert(table_name, key, value) {
+                    Some(v) => Ok(TableManageResult::Value(v.to_vec())),
+                    None => Ok(TableManageResult::None),
+                }
+            },
+            TableManageCmd::Remove { table_name, key } => {
+                match self.remove(table_name, key) {
+                    Some(v) => Ok(TableManageResult::Value(v.to_vec())),
+                    None => Ok(TableManageResult::None),
+                }
+            },
+            TableManageCmd::Drop(name) => {
+                self.drop_table(&name);
+                Ok(TableManageResult::None)
+            },
+        }
     }
 }
