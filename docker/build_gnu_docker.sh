f05cc68cda9fe145267b1b71da9f764aec70deea
heqingpan <heqingpan@126.com>
2024-04-02 00:03:57 +0800

    update build_gnu_docker.sh

--- docker/build_gnu_docker.sh
+++ docker/build_gnu_docker.sh
@@ -5,14 +5,21 @@ echo "TARGETARCH: " $TARGETARCH
 echo "TARGETPLATFORM: " $TARGETPLATFORM
 echo "BUILDPLATFORM: " $BUILDPLATFORM
 
-if ["$TARGETPLATFORM" = "linux/arm64" ];
+CARGO_BUILD_TARGET="x86_64-unknown-linux-gnu"
+if [ "$TARGETPLATFORM" = "linux/arm64" ]; then
+  CARGO_BUILD_TARGET="aarch64-unknown-linux-gnu"
+fi
+
+echo "CARGO_BUILD_TARGET: " $CARGO_BUILD_TARGET
+
+if [ "$TARGETPLATFORM" = "linux/arm64" ];
 then
-  echo "build aarch64-unknown-linux-gnu"
+  echo "build aarch64-unknown-linux-gnu" $TARGETPLATFORM
   rustup target add aarch64-unknown-linux-gnu
   cargo build --release --target aarch64-unknown-linux-gnu
   mv target/aarch64-unknown-linux-gnu/release/rnacos /usr/bin/rnacos
 else
-  echo "build  x86_64-unknown-linux-gnu"
+  echo "build  x86_64-unknown-linux-gnu" $TARGETPLATFORM
   rustup target add x86_64-unknown-linux-gnu
   cargo build --release --target x86_64-unknown-linux-gnu
   mv target/x86_64-unknown-linux-gnu/release/rnacos /usr/bin/rnacos
